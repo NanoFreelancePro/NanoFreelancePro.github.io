5d6f6b3017472867c72490ce8c25c6a968a275f7
NanoFreelancePro <nano.freelance.pro@gmail.com>
2021-12-07 16:09:43 +0100

    on commence le responsive tranquillement

--- public/prestations/prestations.js
+++ public/prestations/prestations.js
@@ -1,16 +1,46 @@
-const color1 = "#0a0252";
-const color8 = "#f0f5ff";
-
 const mouseTarget = document.addEventListener("click", (e) => {
   console.log(e.target);
 });
 const section = document.getElementsByClassName("prestation");
+const active = document.getElementById("active");
+const btn0 = document.getElementById("choice-0");
 const btn1 = document.getElementById("choice-1");
+const btn2 = document.getElementById("choice-2");
+const btn3 = document.getElementById("choice-3");
+const btn4 = document.getElementById("choice-4");
 
+btn0.addEventListener("click", () => {
+  btn0.classList.toggle("toggle-active");
+  btn1.classList.remove("toggle-active");
+  btn2.classList.remove("toggle-active");
+  btn3.classList.remove("toggle-active");
+  btn4.classList.remove("toggle-active");
+});
 btn1.addEventListener("click", () => {
-  btn1.style.background = color8;
-  btn1.style.color = color1;
-  btn1.style.border = "1px solid #0a0252";
-  btn1.style.borderBottom = "none";
-  btn1.style.fontWeight = "bold";
+  btn0.classList.remove("toggle-active");
+  btn1.classList.toggle("toggle-active");
+  btn2.classList.remove("toggle-active");
+  btn3.classList.remove("toggle-active");
+  btn4.classList.remove("toggle-active");
+});
+btn2.addEventListener("click", () => {
+  btn0.classList.remove("toggle-active");
+  btn1.classList.remove("toggle-active");
+  btn2.classList.toggle("toggle-active");
+  btn3.classList.remove("toggle-active");
+  btn4.classList.remove("toggle-active");
+});
+btn3.addEventListener("click", () => {
+  btn0.classList.remove("toggle-active");
+  btn1.classList.remove("toggle-active");
+  btn2.classList.remove("toggle-active");
+  btn3.classList.toggle("toggle-active");
+  btn4.classList.remove("toggle-active");
+});
+btn4.addEventListener("click", () => {
+  btn0.classList.remove("toggle-active");
+  btn1.classList.remove("toggle-active");
+  btn2.classList.remove("toggle-active");
+  btn3.classList.remove("toggle-active");
+  btn4.classList.toggle("toggle-active");
 });
